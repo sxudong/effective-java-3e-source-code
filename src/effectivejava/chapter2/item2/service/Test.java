3971c770d11ba255607d010439d79429d86dc5ab
sxudong <15295901@qq.com>
2020-12-22 23:49:25 +0800

    update

--- src/effectivejava/chapter2/item2/service/Test.java
+++ src/effectivejava/chapter2/item2/service/Test.java
@@ -11,11 +11,14 @@ public class Test {
      * @param args 
      * @throws ClassNotFoundException  
      */  
-    public static void main(String[] args) throws ClassNotFoundException {  
-          
-        Class.forName("cn.netjava.cgl.subway.SubwayProviderImpl");  
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("effectivejava.chapter2.item2.service.SubwayProviderImpl");
         SubWayInterface swi = ServiceManager.getService("一卡通");  
         swi.in();  
         swi.out();  
     }  
 } 
+/* Output:
+通过一卡通进入地铁
+通过一卡通出地铁
+ */
